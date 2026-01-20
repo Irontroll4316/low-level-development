d6265c9c1c75148774fdcba7441930ddede602a4
unknown <irontroll4316@gmail.com>
2026-01-19 23:15:43 -0500

    Scaffold for the options for the program

--- zero-to-hero-in-C/src/main.c
+++ zero-to-hero-in-C/src/main.c
@@ -6,6 +6,41 @@
 // #include "file.h"
 // #include "parse.h"
 
+int print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n - create new database file\n");
+  printf("\t -f - (required) path to database file\n");
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
-  
+  int c;
+  bool newfile = false;
+  char *filepath = NULL;
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) {
+    switch (c) {
+      case 'n':
+        newfile = true;
+        break;
+      case 'f':
+        filepath = optarg;
+        break;
+      case '?':
+        printf("Unkown option -%c\n", c);
+        break;
+      default:
+        return -1;
+    }
+  }
+
+  if (filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+  }
+
+  printf("Newfile: %d\n", newfile);
+  printf("Filepath: %s\n", filepath);
+
+  return 0;
 }
